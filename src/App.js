db59127ee0dacf13a0fa4dc33ef53fc2c75a1014
Saleh Alibrahim <s.albrahim@outlook.com>
2021-12-07 06:33:29 +0300

    feat: 🎸 Push project

--- src/App.js
+++ src/App.js
@@ -1,42 +1,75 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { useState, useEffect } from 'react';
+import { ChakraProvider, theme, Spinner, Center } from '@chakra-ui/react';
 
-function App() {
+import Search from './components/Search';
+import NewsList from './components/news/NewsList';
+
+const App = () => {
+  const [news, setNews] = useState([]);
+  const [searchMessage, setSearchMessage] = useState('React');
+  const [isLoading, setLoading] = useState(true);
+  const [page, setPage] = useState(0);
+  const [maxPage, setMaxPage] = useState(0);
+
+  const onChange = e => {
+    setLoading(true);
+    setPage(0);
+    setSearchMessage(e.target.value);
+  };
+
+  const prefPage = () => {
+    if (page === 0) {
+      return;
+    }
+    setLoading(true);
+    setPage(page - 1);
+  };
+
+  const nextPage = () => {
+    if (page === maxPage) {
+      return;
+    }
+    setLoading(true);
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await fetch(
+          `https://hn.algolia.com/api/v1/search?query=${searchMessage}&page=${page}`
+        );
+        const data = await request.json();
+        setLoading(false);
+        setMaxPage(data.nbPages);
+        setNews(data.hits);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [searchMessage, page]);
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Search
+        searchMessage={searchMessage}
+        isLoading={isLoading}
+        page={page}
+        maxPage={maxPage}
+        onChange={onChange}
+        nextPage={nextPage}
+        prefPage={prefPage}
+      />
+      {isLoading ? (
+        <Center mt="3rem">
+          <Spinner width="10rem" height="10rem" />
+        </Center>
+      ) : (
+        <NewsList news={news} />
+      )}
     </ChakraProvider>
   );
-}
+};
 
 export default App;
